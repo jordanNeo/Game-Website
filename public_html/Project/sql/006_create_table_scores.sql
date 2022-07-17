ce8ad9dc08a4be8099955be460822248f9ac5ece
jordanNeo <jordan.leebt@gmail.com>
2022-07-17 18:43:49 -0400

    save score api

--- public_html/Project/sql/006_create_table_scores.sql
+++ public_html/Project/sql/006_create_table_scores.sql
@@ -1,11 +1,10 @@
 CREATE TABLE IF NOT EXISTS  `Scores`
 (
-    `id`         int auto_increment not null,
+    `id`         int auto_increment PRIMARY KEY,
     `user_id`    int,
-    `score`  INT,
+    `score`  int DEFAULT 0,
     `created`    timestamp default current_timestamp,
     `modified`   timestamp default current_timestamp on update current_timestamp,
-    PRIMARY KEY (`id`),
     FOREIGN KEY (`user_id`) REFERENCES Users(`id`),
-    UNIQUE KEY (`user_id`)
+    check (score > 0)
 )
