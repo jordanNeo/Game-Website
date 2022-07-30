856adc90530feca1d756ac83a95d7673d37d26a1
jordanNeo <jordan.leebt@gmail.com>
2022-07-30 18:10:48 -0400

    credits added and displayed on profile page

--- lib/score_helpers.php
+++ lib/score_helpers.php
@@ -65,6 +65,24 @@ function get_best_score($user_id)
     return 0;
 }
 
+function get_credits($user_id)
+{
+    $query = "SELECT credits from Users WHERE id = :id";
+    $db = getDB();
+    $stmt = $db->prepare($query);
+    try {
+        $stmt->execute([":id" => $user_id]);
+        $r = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($r) {
+            return (int)se($r, "credits", 0, false);
+        }
+    } catch (PDOException $e) {
+        error_log("Error fetching credits for user $user_id: " . var_export($e->errorInfo, true));
+        flash("Error retrieving credits", "danger");
+    }
+    return 0;
+}
+
 function get_latest_scores($user_id, $limit = 10)
 {
     //I'm capping my limit to 1-50
